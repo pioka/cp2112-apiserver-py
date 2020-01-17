a3d6f5f5bfac5ffcbe332139a0c142fb53af9672
pioka <p10mirror2@gmail.com>
2020-01-17 16:13:48 +0900

    WtiteLatch API 実装

--- SLABHIDtoSMBUS.py
+++ SLABHIDtoSMBUS.py
@@ -1,8 +1,87 @@
 import os
+import struct
 from ctypes import *
-
-dllPath = os.path.join(os.path.dirname(os.path.abspath(__file__)), "SLABHIDtoSMBus.dll")
-dll = cdll.LoadLibrary(dllPath)
 
 #dll.HidSmbus_GetNumDevices.restype = c_int
 #dll.HidSmbus_GetNumDevices.argtypes = (byref(c_uint), c_ushort, c_ushort)
+
+class HidSmbus:
+  I2C_RECEIVE_BUFFER_SIZE = c_byte(61)
+
+  def __init__(self):
+    """__init__
+
+    アーキテクチャ(32bit?64bit?)を判定し、対応するDLLをロードする
+    """
+
+    if struct.calcsize(str('P')) * 8 == 64:
+      arch = "x64"
+    else:
+      arch = "x86"
+
+    dllPath = os.path.join(os.path.dirname(os.path.abspath(__file__)), "native", arch, "SLABHIDtoSMBus.dll")
+    self.__dll = cdll.LoadLibrary(dllPath)
+
+  def GetNumDevices(self, refNumDevices:pointer, vid:c_ushort, pid:c_ushort):
+    """GetNumDevices
+      refNumDevice is c_uint pointer
+    """
+
+    ret = self.__dll.HidSmbus_GetNumDevices(refNumDevices, vid, pid)
+    return ret
+
+  def Open(self, refDevice:pointer, deviceNum:c_uint, vid:c_ushort, pid:c_ushort):
+    """Open
+      refDevice is c_int pointer
+    """
+
+    ret = self.__dll.HidSmbus_Open(refDevice, deviceNum, vid, pid)
+    return ret
+
+  def SetGpioConfig(self , refDevice:pointer, direction:c_byte, mode:c_byte, function:c_byte, clkDiv:c_byte):
+    """SetGpioConfig
+    """
+
+    ret = c_int()
+    ret = self.__dll.HidSmbus_SetGpioConfig(refDevice, direction, mode, function, clkDiv)
+    return ret
+
+  def WriteLatch(self, refDevice:pointer, latchValue:c_byte, latchMask:c_byte):
+    """WriteLatch
+    """
+    ret = c_int()
+    ret = self.__dll.HidSmbus_WriteLatch(refDevice, latchValue, latchMask)
+    return ret
+
+  def WriteRequest(self, refDevice:pointer, slaveAddress:c_byte, refBuffer:pointer, numBytesToWrite:c_ushort):
+    """WriteRequest
+    Note:
+      refBuffer: is c_byte pointer
+    """
+
+    ret = self.__dll.HidSmbus_WriteRequest(refDevice, slaveAddress, refBuffer, numBytesToWrite)
+    return ret
+
+  def ReadRequest(self, refDevice:pointer, slaveAddress:c_byte, numBytesToRead:c_ushort):
+    """ReadRequest
+    """
+
+    ret = self.__dll.HidSmbus_ReadRequest(refDevice, slaveAddress, numBytesToRead)
+    return ret
+
+  def ForceReadRequest(self, refDevice:pointer):
+    """ForceReadRequest
+    """
+
+    ret = self.__dll.HidSmbus_ForceReadRequest(refDevice, self.I2C_RECEIVE_BUFFER_SIZE)
+    return ret
+
+  def GetReadResponse(self, refDevice:pointer, refStatus:pointer,  refBuffer:pointer, refNumBytesRead):
+    """GetReadResponse
+      refStatus is c_byte pointer
+      refBuffer is c_byte pointer
+      refNumBytesRead is c_byte pointer
+    """
+
+    ret = self.__dll.HidSmbus_GetReadResponse(refDevice, refStatus, refBuffer, self.I2C_RECEIVE_BUFFER_SIZE, refNumBytesRead)
+    return ret
--- server.py
+++ server.py
@@ -1,10 +1,52 @@
 from ctypes import *
-import SLABHIDtoSMBUS
+from flask import Flask, request
+from flask_cors import CORS
+from SLABHIDtoSMBUS import HidSmbus
 
 CP2112_VID = c_ushort(0x10C4)
 CP2112_PID = c_ushort(0xEA90)
 
-numDevices = c_uint(0)
+cp2112 = HidSmbus()
+devicePtr = pointer(c_int())
+app = Flask(__name__)
+CORS(app)
 
-SLABHIDtoSMBUS.dll.HidSmbus_GetNumDevices(byref(numDevices), CP2112_VID, CP2112_PID)
-print(numDevices)
+@app.route('/')
+def index():
+  return "OK"
+
+@app.route('/WriteLatch')
+def WriteLatch():
+  port = int(request.args.get("port"))
+  value = int(request.args.get("value"))
+
+  if (port < 0 or port > 7):
+    return "Bad Request", 400
+
+  mask = c_byte(1 << port)
+  if (value == 0):
+    cp2112.WriteLatch(devicePtr, c_byte(0x00), mask)
+  else:
+    cp2112.WriteLatch(devicePtr, c_byte(0xFF), mask)
+  return "OK"
+
+
+if __name__ == '__main__':
+  refNumDevices = c_uint()
+  cp2112.GetNumDevices(byref(refNumDevices), CP2112_VID, CP2112_PID)
+
+  if refNumDevices.value != 1:
+    print("ERROR: No Device Connected")
+    exit()
+
+  # Open device
+  ret = cp2112.Open(byref(devicePtr), c_uint(0), CP2112_VID, CP2112_PID)
+  print(ret)
+
+  # Turn on all GPIO port
+  ret = cp2112.SetGpioConfig(devicePtr, c_byte(0xFF), c_byte(0x00), c_byte(0x00), c_byte(0x00))
+  print(ret)
+  ret = cp2112.WriteLatch(devicePtr, c_byte(0x00), c_byte(0xFF))
+  print(ret)
+
+  app.run()
